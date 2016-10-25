c79acbdf052632f811416dc9ca8d3fc2ca4878f7
john ruiz <ruizjm@vcu.edu>
2016-10-25 09:03:04 -0400

    swag

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,11 +1,25 @@
-// ConsoleApplication3.cpp : Defines the entry point for the console application.
-//
+// INFO450multiply.cpp : Defines the entry point for the console application.
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
-
-int main()
+int main(void)
 {
-    return 0;
-}
+	cout << "Multiplication table" << endl;
+	cout << "1\t2\t3\t4\t5\t6\t7\t8\t9\t10" << endl;
 
+	for (int x = 1; x <= 10; x++)
+	{
+		cout << x << "|";
+		for (int y = 1; y < 11; y++)
+		{
+			cout << x*y << "\t";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+;
